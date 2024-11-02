f8b68da9d776c1b139ffc657bb182785edb1bd94
Jean Delpech <84815941+Jehadel@users.noreply.github.com>
2024-11-02 23:47:23 +0100

    Feat : add print_elements method to Queue class (queue.lua)

--- queue.lua
+++ queue.lua
@@ -1,9 +1,9 @@
 -- This module implements a Queue class that behave like a queue
 -- inspired from the book « Programming in Lua » https://www.lua.org/pil/11.4.html
 
-local Object = require 'classic'
+Object = require 'classic'
 
-local Queue = Object:extend()
+Queue = Object:extend()
 
 function Queue:new()
   self.first = 0
@@ -33,6 +33,13 @@ function Queue:dequeue()
     return value
 end
 
+function Queue:print_elements()
+    for i = 0, #queue.data do
+        print(queue.data[i])
+    end
+end
+
+
 --[[test 
 queue = Queue()
 print('enqueue A')
@@ -42,10 +49,8 @@ queue:enqueue('B')
 print('enqueue C')
 queue:enqueue('C')
 
-print('Elements enqueued:')
-for i = 0, #queue.data do
-    print(queue.data[i])
-end
+queue:print_elements()
+
 print('queue.first', queue.first)
 print('queue.last', queue.last)
 print('queue empty', queue.empty)
@@ -61,10 +66,7 @@ print('queue.last', queue.last)
 print('queue empty', queue.empty)
 
 
-print('Elements enqueued:')
-for i = 0, #queue.data do
-    print(queue.data[i])
-end
+queue:print_elements()
 
 queue:enqueue('A')
 print(queue.data[queue.first])
